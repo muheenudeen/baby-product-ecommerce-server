afc3d66e597d4a6286ff9adec7cee190cb4a6029
muheenudeen <muheenudeen313@gmail.com>
2024-09-21 17:58:02 +0530

    update

--- Controller/userSide/userController/userController.js
+++ Controller/userSide/userController/userController.js
@@ -1,23 +1,28 @@
-import bcrypt from "bcryptjs"
 import User from "../../../Model/userSchema/userSchema.js";
-
+import { bcryptData } from "../../../Utils/bcrypt.js";
 
 const signUp = async (req, res) => {
     try {
         
-        const hashedPassword = await bcrypt.hash(req.body.password,10);
 
         const user = new User ({
             fname:req.body.fname,
             sname:req.body.sname,
             email:req.body.email,
-            password:hashedPassword
+            password:bcryptData.hashedPassword,
         })
+
+        const existingUser = await userSchema.findOne({email})
+        if(existingUser){
+            return res
+            .status(400)
+            .json({success:false , message:"Email already exist..."})
+        }
 
         await user.save();
         res.status(201).json(user);
     } catch (error) {
-        res.status(500).json({ message: "Error signing up", error: error.message });
+        res.status(500).json({ message: "typing error"});
     }
 };
 
